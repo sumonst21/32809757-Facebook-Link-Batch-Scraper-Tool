9e9ed16e1de750c1996e98d1abd6d349a5d83145
Md. Sumon Islam <sumonst21@gmail.com>
2022-02-09 23:21:51 +0600

    Update usage.php

--- usage.php
+++ usage.php
@@ -10,29 +10,41 @@ if (!empty($accessToken)) {
 
 // get urls from file
 $urls = urls_from_file('urls.txt');
+if (!$urls)
+    die('No urls to reload');
 
 // process urls
 foreach ($urls as $url) {
-	$result = $fb->reload($url);
-	if ($result) {
-		$result = json_decode($result, true);
-		if (isset($result['error'])) {
-			echo 'Error: ' . $result['error']['message'] . PHP_EOL;
-		} else {
-			echo 'Success: ' . $url . PHP_EOL;
-		}
-	}
+    try {
+        $result = $fb->reload($url);
+        if ($result) {
+            $result = json_decode($result, true);
+            if (isset($result['error'])) {
+                echo 'Error: ' . $result['error']['message'] . PHP_EOL;
+            } else {
+                echo 'Success: ' . $url . PHP_EOL;
+            }
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        if ($e->getMessage() == 'FACEBOOK_ACCESS_TOKEN is not set') {
+            break;
+        }
+    }
 }
 
 /**
  * Read urls from file
  *
- * @param  string $file
+ * @param string $file
  *
- * @return array         
+ * @return array|false
  * @author sumonst21 <sumonst21@gmail.com>
  */
-function urls_from_file($file) {
+function urls_from_file(string $file) {
+    if (!file_exists($file)) {
+        return false;
+    }
     $urls = array();
     $handle = fopen($file, "r");
     if ($handle) {
@@ -41,8 +53,8 @@ function urls_from_file($file) {
         }
         fclose($handle);
     } else {
+        return false;
         // error opening the file.
     }
-    return $urls;
+    return ($urls) ?: array();
 }
-?>
